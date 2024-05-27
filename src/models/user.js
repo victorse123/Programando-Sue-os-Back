40a416a6c32c2fa243b60b45d234bf2cb59dc95c
Victor S <victorseva123@gmail.com>
2024-05-27 14:42:33 -0500

    Back

---
+++ src/models/user.js
@@ -0,0 +1,50 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "User",
+    {
+      id:{type:DataTypes.INTEGER,
+        autoIncrement:true,
+      primaryKey:true
+    }
+    ,
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true // Asegura que el valor de email sea una dirección de correo electrónico válida
+        }
+    },
+    imageDefault: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'user',
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    DNI: {
+      type: DataTypes.STRING,
+      //allowNull: false,
+      //unique: true,
+  },
+  dateOfBirth: {
+    type: DataTypes.STRING,
+    //allowNull: false,
+    //unique: true
+},
+     
+    },
+    {  timestamps: false} );
+};
